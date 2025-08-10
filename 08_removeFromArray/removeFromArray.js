178bd9822d7050e1eef3c95c2d23e5b22c3a2365
ataur <atauraahmad@gmail.com>
2025-08-10 17:39:17 +0100

    do all JS exercises sofar

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...b) {
+    // const newArr = [];
+
+    // arr.forEach(item => {
+    //     if (!b.includes(item)) {
+    //         newArr.push(item);
+    //     }
+    // })
+    // return newArr;
+
+    return arr.filter(item => !b.includes(item));
+
 };
 
 // Do not edit below this line
